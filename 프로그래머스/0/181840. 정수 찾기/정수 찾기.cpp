0c5afb744377a44a90a276327746cad093b93d44
Yang Minjune <96441874+juneisjune@users.noreply.github.com>
2024-01-31 15:20:50 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int solution(vector<int> num_list, int n) {
     int answer = 0;
     for(int i=0;i<num_list.size();i++){
-        string tempstring=to_string(num_list[i]);
-        int temp=stoi(tempstring);
-        if(temp==n)
+        
+        if(num_list[i]==n)
             return 1;
     }
     return 0;
