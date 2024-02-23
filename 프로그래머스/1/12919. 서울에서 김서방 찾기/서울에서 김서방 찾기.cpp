de4cb4551f7cbc93ee5a3562984227666a586b09
Yang Minjune <96441874+juneisjune@users.noreply.github.com>
2024-02-24 00:01:12 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -7,9 +7,8 @@ string solution(vector<string> seoul) {
     string answer = "";
     for(int i=0;i<seoul.size();i++){
         if(seoul[i]=="Kim"){
-            answer+="김서방은 ";
-            answer+=to_string(i);
-            answer+="에 있다";
+            answer="김서방은 "+to_string(i)+"에 있다";
+           
             return answer;
         }
             
