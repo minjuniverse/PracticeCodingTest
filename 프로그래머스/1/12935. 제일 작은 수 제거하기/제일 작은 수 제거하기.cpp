4e2ba16b4c20d5389a21c02d4fcf6d206ab36d53
Yang Minjune <96441874+juneisjune@users.noreply.github.com>
2024-03-21 22:17:10 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.30 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -6,28 +6,26 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
-
-    // 배열이 하나인 경우 -1을 추가하고 반환
-    if(arr.size() == 1){
+    if(arr.size()==1){
         answer.push_back(-1);
         return answer;
     }
         
-    // minValue, minIndex 초기화
-    int minValue = arr[0];
-    int minIndex = 0;
-
-    // 최소값과 인덱스 찾기
-    for(int i = 1; i < arr.size(); i++) {
-        if(arr[i] < minValue) {
-            minValue = arr[i];
-            minIndex = i;
+    int minValue=arr[0];
+    int minIndex=0;
+    for(int i=1;i<arr.size();i++){
+        if(arr[i]<minValue)
+        {
+            minValue=arr[i];
+            minIndex=i;
         }
+           
     }
  
-    // 최소값 제거
-    arr.erase(arr.begin() + minIndex);
-
-    // 수정된 배열 반환
+    
+    arr.erase(arr.begin()+minIndex);
+    //for(int i=0;i<arr.size();i++){
+      //  answer.push_back(arr[i]);
+    //}
     return arr;
 }
